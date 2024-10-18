f39746f06adda69e394be6a4dc63e2274fe6554a
Madhan-Mohan-Reddy-Avula <142919344+Madhan-Mohan-Reddy-Avula@users.noreply.github.com>
2024-10-18 21:56:35 +0530

    Add files via upload
    website

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting Repair.in! We will get back to you soon.');
+});
